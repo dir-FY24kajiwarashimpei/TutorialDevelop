bbf2ff51faa19f90ae7869ce8be4a010b316ef9a
dir-FY24kajiwarashimpei <shinpei.kajiwara.h902@dirbato.co.jp>
2024-04-25 14:17:40 +0900

    入力チェック機能を実装

--- TutorialDevelop/src/main/java/com/techacademy/entity/User.java
+++ TutorialDevelop/src/main/java/com/techacademy/entity/User.java
@@ -9,6 +9,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import jakarta.validation.constraints.Email; 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 
 
 @Data
@@ -31,21 +39,28 @@ public class User {
     
     /**名前。２０桁。null不許可*/
     @Column(length = 20, nullable = false)
+    @NotEmpty
+    @Length(max=20)
     private String name;
     
     
     /**性別。２桁。列挙型（文字列）*/
     @Column(length = 2)
     @Enumerated(EnumType.STRING)
+    @NotNull
     private Gender gender;
     
     
     /**年齢 */
+    @Min(0)
+    @Max(120)
     private Integer age;
     
     
     /**メールアドレス。５０桁。null許可 */
     @Column(length = 50)
+    @Email
+    @Length(max=50)
     private String email;
 
     
